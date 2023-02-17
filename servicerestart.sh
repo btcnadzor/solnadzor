0c8fd14d93796f2e5023a1aa9410e086436a2099
btcnadzor <33119230+btcnadzor@users.noreply.github.com>
2023-02-17 18:54:35 +0300

    Update servicerestart.sh

--- servicerestart.sh
+++ servicerestart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -A | grep solana || systemctl stop solana && rm -rf /root/ledger/* && fstrim -av && source $HOME/solana-snapshot-finder/venv/bin/activate && python3 /root/solana-snapshot-finder/snapshot-finder.py --snapshot_path /root/ledger --max_download_speed 90 --min_download_speed 20 --num_of_retries 111 && systemctl start solana
+ps -A | grep solana || sudo systemctl stop solana && sudo rm -rf /root/ledger/* && fstrim -av && source $HOME/solana-snapshot-finder/venv/bin/activate && python3 /root/solana-snapshot-finder/snapshot-finder.py --snapshot_path /root/ledger --max_download_speed 90 --min_download_speed 20 --num_of_retries 111 && sudo systemctl start solana
