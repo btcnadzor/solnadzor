3a981968d9ec9129f7d3a0acad293da7e681b6f9
btcnadzor <33119230+btcnadzor@users.noreply.github.com>
2023-02-17 19:23:17 +0300

    Update and rename servicerestart1.sh to bootstart.sh

--- servicerestart1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ps -A | grep solana || sudo systemctl stop solana && sudo rm -rf /root/ledger/* && fstrim -av && source $HOME/solana-snapshot-finder/venv/bin/activate && python3 /root/solana-snapshot-finder/snapshot-finder.py --snapshot_path /root/ledger --max_download_speed 90 --min_download_speed 20 --num_of_retries 111 && sudo systemctl start solana
