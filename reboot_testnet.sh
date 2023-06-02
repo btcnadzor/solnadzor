f97bc8a8aed366c23458d675cd34860e1cbf6eb4
btcnadzor <33119230+btcnadzor@users.noreply.github.com>
2023-06-02 18:11:24 +0300

    Update reboot_testnet.sh

--- reboot_testnet.sh
+++ reboot_testnet.sh
@@ -3,6 +3,7 @@ echo 'REBOOT testnet'
 systemctl stop solana
 pausemin=15
 read -t 10 -p "сколько подождать до перезагрузки (15мин) " pausemin
+date -d "+3 hours" +%H:%M
 let "pausesec = $pausemin * 60"
 sleep $pausesec
 echo 'solana rebooting'
@@ -13,4 +14,5 @@ source $HOME/solana-snapshot-finder/venv/bin/activate
 python3 /root/solana-snapshot-finder/snapshot-finder.py --snapshot_path /root/ledger -r https://api.testnet.solana.com --max_download_speed 90 --min_download_speed 20 --num_of_retries 5
 systemctl start solana
 deactivate
+date -d "+3 hours" +%H:%M
 exit
