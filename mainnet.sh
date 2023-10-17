c848013ac8097cd212c570fa5c5668d6a4909003
btcnadzor <33119230+btcnadzor@users.noreply.github.com>
2023-10-17 14:27:31 +0300

    Update and rename bootstatup2.sh to mainnet.sh

--- mainnet.sh
+++ mainnet.sh
@@ -10,6 +10,7 @@ if [[ $solanawork == 0 ]]
 		echo 'solana rebooting'
 		systemctl stop solana 
 		rm -rf /root/ledger/*
+  		cp /root/genesis.bin /root/ledger/genesis.bin
 		fstrim -av 
 		source $HOME/solana-snapshot-finder/venv/bin/activate
 		python3 /root/solana-snapshot-finder/snapshot-finder.py --snapshot_path /root/ledger --max_download_speed 90 --min_download_speed 20 --num_of_retries 5
