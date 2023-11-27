e3f87ec5ae3407d2757267894ca5b4dd58eb1d04
btcnadzor <33119230+btcnadzor@users.noreply.github.com>
2023-11-27 23:41:49 +0300

    Update update_main.sh

--- update_main.sh
+++ update_main.sh
@@ -3,6 +3,7 @@ sudo solana-sys-tuner --user $(whoami) > sys-tuner.log 2>&1 &
 solana-validator wait-for-restart-window && sudo systemctl stop solana
 sudo rm -rf /root/ledger/*
 fstrim -av
+cp /root/genesis.bin /root/ledger/genesis.bin
 source $HOME/solana-snapshot-finder/venv/bin/activate && python3 /root/solana-snapshot-finder/snapshot-finder.py --snapshot_path /root/ledger --max_download_speed 90 --min_download_speed 20 --num_of_retries 555
 sudo systemctl start solana
 deactivate
